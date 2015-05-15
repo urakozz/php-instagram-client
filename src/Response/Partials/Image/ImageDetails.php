921d9b3337cb4880ec49b78fb027c67166c6e65e
urakozz <urakozz@gmail.com>
2015-05-15 16:12:52 +0200

    annotations

--- src/Response/Partials/Image/ImageDetails.php
+++ src/Response/Partials/Image/ImageDetails.php
@@ -1,20 +1,20 @@
 <?php
-/**
- * PHP Version 5
- *
- * @category  H24
- * @package   
- * @author    "Yury Kozyrev" <yury.kozyrev@home24.de>
- * @copyright 2015 Home24 GmbH
- * @license   Proprietary license.
- * @link      http://www.home24.de
- */
 
 namespace Instagram\Response\Partials\Image;
 
-
 use JMS\Serializer\Annotation\Type;
 
+/**
+ * PHP Version 5
+ *
+ * Class ImageDetails
+ *
+ * @package   Instagram\Response\Partials\Image
+ * @author    "Yury Kozyrev" <urakozz@gmail.com>
+ * @copyright 2015 "Yury Kozyrev"
+ * @license   MIT
+ * @link      https://github.com/urakozz/php-instagram-client
+ */
 class ImageDetails
 {
 
