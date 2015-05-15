b6fda87bc388eef154b9c963af6148db997765fa
urakozz <urakozz@gmail.com>
2015-05-15 09:47:13 +0200

    client_

--- src/Client/InstagramClient.php
+++ src/Client/InstagramClient.php
@@ -2,16 +2,27 @@
 /**
  * PHP Version 5
  *
- * @package   
+ * @package
  * @author    "Yury Kozyrev" <urakozz@gmail.com>
- * @copyright 2015 "Yury Kozyrev" 
+ * @copyright 2015 "Yury Kozyrev"
  * @license   MIT
  * @link      https://github.com/urakozz/php-instagram-client
  */
 
 namespace Instagram\Client;
 
-
-class InstagramClient {
+/**
+ * PHP Version 5
+ *
+ * Class InstagramClient
+ *
+ * @package Instagram\Client
+ * @author    "Yury Kozyrev" <urakozz@gmail.com>
+ * @copyright 2015 "Yury Kozyrev"
+ * @license   MIT
+ * @link      https://github.com/urakozz/php-instagram-client
+ */
+class InstagramClient
+{
 
 }
