911f9c2b36c66843d52690407c0cf9b982118c74
TzepART2 <killzooona@mail.ru>
2015-06-07 23:55:27 +0300

    Create GetUserFollowsRequest

---
+++ src/Request/Relationships/GetUserFollowsRequest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artem
+ * Date: 07.06.15
+ * Time: 23:17
+ */
+
+namespace Instagram\Request\Relationships;
+
+
+use Instagram\Request\AbstractInstagramRequest;
+use Instagram\Response\Users\UserResponse;
+use Instagram\Response\AbstractInstagramResponse;
+
+
+class GetUserFollowsRequest extends AbstractInstagramRequest
+{
+    public function setUserId($userId)
+    {
+        $this['user_id'] = $userId;
+    }
+
+    /**
+     * Get Request Method (GET|POST|DELETE)
+     *
+     * @return string
+     */
+    public function getMethod()
+    {
+        return "GET";
+    }
+
+    /**
+     * Get Request URL
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return "/user/{user_id}";
+    }
+
+    /**
+     * Get required attributes
+     *
+     * @return mixed
+     */
+    public function getRequiredAttributes()
+    {
+        return ['user_id'];
+    }
+
+    /**
+     * Get Response Prototype
+     *
+     * @return AbstractInstagramResponse
+     */
+    public function getResponsePrototype()
+    {
+
+    }
+}
